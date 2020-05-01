79a031460a4a2a4a52884f66fdd6aba09b87481e
Mohit Chowdhury <shadhin.aust@gmail.com>
2020-05-01 08:17:25 +0600

    added functionality

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ var slots map[int]carDetails
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print(">")
 	for scanner.Scan() {
 		cmd := strings.Split(scanner.Text(), " ")
 		if cmd[0] == "exit" {
@@ -47,20 +48,22 @@ func main() {
 			printStatus()
 		case regNumsForColor:
 			color := cmd[1]
-			fmt.Println(color)
+			findRegNumsByColor(color)
 		case slotNumsForColor:
 			color := cmd[1]
-			fmt.Println(color)
+			findSlotNumsByColor(color)
 		case slotNumForRegNum:
 			regNum := cmd[1]
-			fmt.Println(regNum)
+			findSlotNoByRegNum(regNum)
 		}
+		fmt.Print(">")
 	}
 }
 
 func parkCar(regNum, color string) {
 	slotsLen := len(slots) + 1
 	if slotsLen > slotNum {
+		fmt.Println("No slot found!")
 		return
 	}
 	slots[slotsLen] = carDetails{
@@ -76,6 +79,33 @@ func leaveParking(slotNum int) {
 func printStatus() {
 	fmt.Println("Slot\tRegistration Number\tColor")
 	for slot, carDetails := range slots {
-		fmt.Printf("%v\t%v\t%v\n", slot, carDetails.regNum, carDetails.color)
+		fmt.Printf("%-6v\t%-20s\t%-16s\n", slot, carDetails.regNum, carDetails.color)
 	}
+}
+
+func findRegNumsByColor(color string) {
+	for _, carDetails := range slots {
+		if strings.EqualFold(carDetails.color, color) {
+			fmt.Print(carDetails.regNum, " ")
+		}
+	}
+	fmt.Println()
+}
+
+func findSlotNumsByColor(color string) {
+	for slot, carDetails := range slots {
+		if strings.EqualFold(carDetails.color, color) {
+			fmt.Print(slot, " ")
+		}
+	}
+	fmt.Println()
+}
+
+func findSlotNoByRegNum(regNum string) {
+	for slot, carDetails := range slots {
+		if strings.EqualFold(carDetails.regNum, regNum) {
+			fmt.Print(slot, " ")
+		}
+	}
+	fmt.Println()
 }
